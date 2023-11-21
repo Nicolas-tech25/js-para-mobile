b6ef23d85a6298233e695456c0408fd9f6efbcbd
Nicolas_Amorim <ni.oliveiraamorim.25@gmail.com>
2023-11-21 09:28:01 -0300

    diferença de const e let e var

---
+++ 01-var-let-const.js
@@ -0,0 +1,20 @@
+// 01 var-let-const
+
+// var idade = 18;
+// let mensagem;
+// if( idade >= 18 ){
+//     mensagem = "é adulto";
+// }else{
+//     mensagem = "é adulto";
+// }
+
+// console.log(idade);
+// console.log(mensagem);
+
+let meuNome = "Nicolas";
+console.log(meuNome);
+
+meuNome = "Nicolas Oliveira";
+console.log(meuNome);
+
+const escola = "Senac";
